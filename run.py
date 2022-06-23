4e801ab05566336f002366e344a99c24f3dfabd4
Matt <mjd014@live.co.uk>
2022-06-23 10:39:35 +0000

    modified run.py

--- run.py
+++ run.py
@@ -7,9 +7,11 @@ SCOPE = [
     "https://www.googleapis.com/auth/drive"
     ]
 
-CREDS = credentials.from_service_account_file('creds.json')
+CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
 sales = SHEET.worksheet('sales')
+data = sales.get_all_values()
+print(data)
