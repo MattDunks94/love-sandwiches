3cb304d4cdd87316be81fe75c5777c2aa4f86fec
Matt <mjd014@live.co.uk>
2022-06-23 12:35:29 +0000

    add user request loop to repeat request for data until valid data is provided

--- run.py
+++ run.py
@@ -30,6 +30,9 @@ def get_sales_data():
             print("Data is valid!")
             break
 
+    return sales_data
+
+
 def validate_data(values):
     """
     Inside the try, converts all string values into integers.
@@ -47,4 +50,5 @@ def validate_data(values):
         return False
 
     return True
-get_sales_data()
+    
+data = get_sales_data()
